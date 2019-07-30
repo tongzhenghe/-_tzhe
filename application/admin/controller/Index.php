745b3701aa09197ec513d15e0fda22aed7bbcf3c
tzhe <805578494@qq.com>
2019-07-30 08:45:45 +0800

    抄送我的

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -103,7 +103,7 @@ class Index extends Common
         }
 
 
-        $data = Db::name($table)->where(['state' => 1, 'compid' => $compId ])->order('sort asc')->select();
+        $data = Db::name($table)->where(['state' => 1])->order('sort asc')->select();
         $data = tree($data);
 wl_debug($data);
         return view('' , ['data' => $data]);
