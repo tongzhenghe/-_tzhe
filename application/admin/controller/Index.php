3cf0923cbd53d1c12c0eec791ca0ee891d79641d
tzhe <805578494@qq.com>
2019-07-31 15:37:08 +0800

    抄送我的

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -76,7 +76,6 @@ class Index extends Common
             }
 
             $general = Db::name($table)->where(['state' => 1, 'compid' => $compId ])->field('id, pid, name')->select();
-            wl_debug($general);
             $general = tree($general);
 
 
