8083b8a5facee24067207e0400a796339d070f1f
tzhe <805578494@qq.com>
2019-08-02 10:53:01 +0800

    抄送我的；·

--- public/static/admin/js/zself.js
+++ public/static/admin/js/zself.js
@@ -195,7 +195,7 @@ $(function () {
     $(".select-user").on("click", '.toggle-on', function () {
         //获取当前id。 并和左侧比对， 如果相同就删除
        var right_id = $(this).parent('span').attr('data-id')
-           ,id_arr = $('.center-users').prev().children().children().children('p');
+           ,id_arr = $('.center-users').prev().children().children().children('p').attr('data-id');
            //获取左边所有id
         console.log(id_arr)
            // $('.center-users').prev(),
