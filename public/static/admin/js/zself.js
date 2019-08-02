c7a9f8bd4f1415316544d9c44d8c83ef8a4d056d
tzhe <805578494@qq.com>
2019-08-02 10:52:19 +0800

    抄送我的；·

--- public/static/admin/js/zself.js
+++ public/static/admin/js/zself.js
@@ -195,10 +195,10 @@ $(function () {
     $(".select-user").on("click", '.toggle-on', function () {
         //获取当前id。 并和左侧比对， 如果相同就删除
        var right_id = $(this).parent('span').attr('data-id')
-           ,id_arr = $('.center-users').prev().children();
+           ,id_arr = $('.center-users').prev().children().children().children('p');
            //获取左边所有id
-           // $('.center-users').prev(),
         console.log(id_arr)
+           // $('.center-users').prev(),
 
 
 
