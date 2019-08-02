05050cd0c9ced8e76cdc71ce396ec5c012cbbcb0
tzhe <805578494@qq.com>
2019-08-02 11:03:53 +0800

    抄送我的；·

--- public/static/admin/js/zself.js
+++ public/static/admin/js/zself.js
@@ -197,13 +197,16 @@ $(function () {
         //获取当前id。 并和左侧比对， 如果相同就删除
        var   id_arr = [],right_id = $(this).parent('span').attr('data-id');
        $.each($('.center-users').prev().children().children().children('p'), function (k, v) {
-          console.log(v)
+           id_arr.push($(v).attr('data-id'));
        });
 
        //获取左边所有id
-       //  console.log(id_arr);
-       //  console.log(right_id);
-       //  console.log($.inArray( right_id, id_arr ))
+
+       if ($.inArray( right_id, id_arr )) {
+           alert(3)
+       }  else {
+           alert(1)
+       }
        // $('.center-users').prev(),
 
 
