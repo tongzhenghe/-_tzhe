c3fd269d20616c2f56c13e133bf0c6668db42f77
tzhe <805578494@qq.com>
2019-08-04 14:58:51 +0800

    抄送我的；·

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -86,10 +86,9 @@ class Index extends Common
                     $row['settinged_appro_member'] = (array)json_decode($row['settinged_appro_member']);
                     $row['settinged_appro_member'] = implode(",", $row['settinged_appro_member']);
                     $row['settinged_appro_member'] = explode(",", $row['settinged_appro_member']);
-                    wl_debug($row['settinged_appro_member']);
 
                     $dd = [];
-                    for($i = 1; $i<=count( $row['settinged_appro_member']); $i++) {
+                    for($i = 1; $i<count( $row['settinged_appro_member']); $i++) {
                        $dd[] = Db::name('user')->where('id', $row['settinged_appro_member'][$i])->find();
                     }
 
