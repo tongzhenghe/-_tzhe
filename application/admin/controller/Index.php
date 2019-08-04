d0b4f7d6ee505fce6528b0d908922a060fafbc8e
tzhe <805578494@qq.com>
2019-08-04 15:02:01 +0800

    抄送我的；·

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -89,10 +89,10 @@ class Index extends Common
 
                     for($i = 0; $i<count( $row['settinged_appro_member']); $i++) {
 
-                        $row['settinged_appro_members'][] = Db::name('user')->where('id', $row['settinged_appro_member'][$i])->find();
+                        $row['settinged_appro_member'][] = Db::name('user')->where('id', $row['settinged_appro_member'][$i])->find();
                     }
 
-                    wl_debug(  $row['settinged_appro_members']);
+                    wl_debug(  $row['settinged_appro_member']);
                 }
             }
 
