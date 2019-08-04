e819e8a2165d4f4160bd8ee29a625a5e07314f23
tzhe <805578494@qq.com>
2019-08-04 17:15:43 +0800

    抄送我的；·

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -44,8 +44,8 @@ class Index extends Common
                 if (empty($post['url'])) exit(iJson('', 400, 400, '请填写请求地址'));
 
                 //处理自定义审批人
-                $custom_type = 2;
-                $custom_type = intval($post['custom_type']);
+                $custom_type = !empty($post['custom_type']) ? $post['custom_type'] : 2;
+
                 $app_people = null;
                 if ($custom_type == 1 ) {
                     $app_people = array_filter($post['app_people']);
