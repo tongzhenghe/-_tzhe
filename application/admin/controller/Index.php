50ba7df1fd3de95a1f8c7a9a9e6ac517ac40a642
tzhe <805578494@qq.com>
2019-08-04 15:00:32 +0800

    抄送我的；·

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -85,7 +85,7 @@ class Index extends Common
                 if (!empty($row['settinged_appro_member'])) {
                     $row['settinged_appro_member'] = (array)json_decode($row['settinged_appro_member']);
                     $row['settinged_appro_member'] = implode(",", $row['settinged_appro_member']);
-                    $row['settinged_appro_member'] = array_filter(explode(",", $row['settinged_appro_member']));
+                    $row['settinged_appro_member'] = explode(",", $row['settinged_appro_member']);
 
                     wl_debug(count( $row['settinged_appro_member']));
                     for($i = 1; $i<count( $row['settinged_appro_member']); $i++) {
