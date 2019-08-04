1a531a1ed03d134d4321f2d498ab21c362732116
tzhe <805578494@qq.com>
2019-08-04 14:38:11 +0800

    抄送我的；·

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -86,7 +86,6 @@ class Index extends Common
                     $row['settinged_appro_member'] = json_decode($row['settinged_appro_member']);
 
                     for($i = 1; $i<count( $row['settinged_appro_member']); $i++) {
-
                         wl_debug($row['settinged_appro_member'][$i]);
 
                     }
