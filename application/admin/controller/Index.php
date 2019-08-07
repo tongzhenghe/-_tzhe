f7217ce8072fc71b3fa7fbd9924d4402272dfe3b
tzhe <805578494@qq.com>
2019-08-07 10:38:44 +0800

    通用审批详情完善

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -200,6 +200,7 @@ class Index extends Common
 
                         $tysp['create_time'] = timeTran($tysp['create_time']);
 
+                        wl_debug($tysp['id']);
                             $tysp['approval_user'] = Db::name('appprostate')
                                 ->where('approval_id', intval($tysp['id']))
                                 ->order('appro_sort asc')
