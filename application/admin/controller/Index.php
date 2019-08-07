325999b206f6462c751e237a157e4dbb7aa82eaa
tzhe <805578494@qq.com>
2019-08-07 17:04:23 +0800

    通用审批详情完善

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -574,7 +574,7 @@ class Index extends Common
                             $vs['annex'] = unserialize($vs['annex']);
                         }
 
-                        wl_debug(unserialize('a:1:{i:0;s:55:"http://huamei.juhuiny.com/2019/08/556d7201908071841.png";}'));
+                        wl_debug($gzsp_specs);
 
 
                         $gzsp['salary'] = $gzsp_specs;
