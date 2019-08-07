d7eba070b5ff3e78726b2613e30fdd06ff6655a3
tzhe <805578494@qq.com>
2019-08-07 16:10:16 +0800

    通用审批详情完善

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -588,11 +588,13 @@ class Index extends Common
                     break;
 
             }
-
+]
+            wl_debug($compid);
             $user = Db::name('user a')
                 ->join('department b', 'a.department_id = b.id', 'left')
                 ->field('a.user_name, b.name department_name')
                 ->where('a.id', $v['send_user_id'])
+                ->where('a.id', $compid)
                 ->find();
             wl_debug($user);
 
