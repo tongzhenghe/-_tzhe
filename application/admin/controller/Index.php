f7a0bd8159a73474e89a2f365e4c49d7c6a24811
tzhe <805578494@qq.com>
2019-08-07 15:06:02 +0800

    通用审批详情完善

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -565,6 +565,7 @@ class Index extends Common
 
         //支付方式
         $data_pay = Db::name($table_type)->where('compid', $compid)->select();
+        wl_debug($data_pay);
         foreach ($data_pay as &$vs ) {
             $vs['time'] = date('Y/m/d H:i:s', $vs['time']);
         }
